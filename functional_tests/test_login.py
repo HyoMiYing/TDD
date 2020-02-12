03318360c96cd09dd2b849580f13f54fd627dbe7
Rok Klancar <rklancar@gmail.com>
2020-02-12 14:49:50 +0100

    Refactored some FTs when looking up Harry's repo on Github

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -12,6 +12,36 @@ SUBJECT = 'Your login link for Superlists'
 
 class LoginTest(FunctionalTest):
 
+    def wait_for_email(self, test_email, subject):
+        if not self.staging_server:
+            email = mail.outbox[0]
+            self.assertIn(test_email, email.to)
+            self.assertEqual(email.subject, subject)
+            return email.body
+
+        email_id = None
+        start = time.time()
+        inbox = poplib.POP3_SSL('pop.mail.yahoo.com')
+        try:
+            inbox.user(test_email)
+            inbox.pass_('wtenyuaiqhwfsjnl')
+            while time.time() - start < 60:
+                # Get 10 newest messages
+                count, _ = inbox.stat()
+                for i in reversed(range(max(1, count - 10), count + 1)):
+                    print('getting msg', i)
+                    _, lines, __ = inbox.retr(i)
+                    lines = [l.decode('utf8') for l in lines]
+                    if f'Subject: {subject}' in lines:
+                        email_id = i
+                        body = '\n'.join(lines)
+                        return body
+                time.sleep(5)
+        finally:
+            if email_id:
+                inbox.dele(email_id)
+            inbox.quit()
+
     def test_can_get_email_link_to_log_in(self):
         # Edith goes to the awesome superlists site
         # and notices a "Log in" section in the navbar for the first time
@@ -44,6 +74,7 @@ class LoginTest(FunctionalTest):
 
         # she clicks it
         self.browser.get(url)
+        print(url)
 
         # she is logged in!
         self.wait_to_be_logged_in(email=test_email)
@@ -53,33 +84,3 @@ class LoginTest(FunctionalTest):
 
         # She is logged out
         self.wait_to_be_logged_out(email=test_email)
-
-    def wait_for_email(self, test_email, subject):
-        if not self.staging_server:
-            email = mail.outbox[0]
-            self.assertIn(test_email, email.to)
-            self.assertEqual(email.subject, subject)
-            return email.body
-
-        email_id = None
-        start = time.time()
-        inbox = poplib.POP3_SSL('pop.mail.yahoo.com')
-        try:
-            inbox.user(test_email)
-            inbox.pass_('wtenyuaiqhwfsjnl')
-            while time.time() - start < 60:
-                # Get 10 newest messages
-                count, _ = inbox.stat()
-                for i in reversed(range(max(1, count - 10), count + 1)):
-                    print('getting msg', i)
-                    _, lines, __ = inbox.retr(i)
-                    lines = [l.decode('utf8') for l in lines]
-                    if f'Subject: {subject}' in lines:
-                        email_id = i
-                        body = '\n'.join(lines)
-                        return body
-                time.sleep(5)
-        finally:
-            if email_id:
-                inbox.dele(email_id)
-            inbox.quit()
